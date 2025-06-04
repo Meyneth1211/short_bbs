8ddfef52aa19ef31e99a0d316c31bf6dac55eaf2
Meyneth1211 <2301389@s.asojuku.ac.jp>
2025-06-04 15:29:17 +0900

    Update check.php

--- check.php
+++ check.php
@@ -2,7 +2,7 @@
 session_start();
     
 function displayname(){
-    if ($_SESSION['userid']) {
+    if (!empty($_SESSION['userid'])) {
         echo '<p>ようこそ、'.htmlspecialchars($_SESSION['username']).'さん！</p>';
     } else {
         echo '<p>ようこそ、ゲストさん！</p>';
